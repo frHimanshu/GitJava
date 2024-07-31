de386ee92174525ef0ba7600f25f7e5a6f15c68b
frHimanshu <himanshu2006garode@gmail.com>
2024-07-31 16:28:28 +0530

    Fixed Files

---
+++ 8-Array (Part I)/theory/create.java
@@ -0,0 +1,12 @@
+package theory;
+import java.util.*;
+
+public class create {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        
+        int numbers[] = {1, 2, 3};  // Added semicolon
+
+        String fruits[] = {"apple", "mango", "chikkki"};  // Added semicolon
+    }
+}
