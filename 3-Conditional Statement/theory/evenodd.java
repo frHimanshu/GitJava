4640b9fe355021088008ce1d31c376600444308c
frHimanshu <himanshu2006garode@gmail.com>
2024-10-07 21:03:50 +0530

    Fixed

--- 3-Conditional Statement/theory/evenodd.java
+++ 3-Conditional Statement/theory/evenodd.java
@@ -1,22 +1,19 @@
-// package 3-Conditional Statement;
 import java.util.Scanner;
-
 
 public class evenodd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Float num = sc.nextFloat();
-        num = num / 2;
+        // Read an integer input from the user
+        int num = sc.nextInt();
 
-
-
-        if(num == 0 || num < 0){
+        // Check if the number is even or odd using modulus
+        if (num % 2 == 0) {
             System.out.print("Even");
         } else {
             System.out.print("Odd");
         }
 
+        sc.close(); // Close the scanner to avoid resource leak
     }
-    
 }
